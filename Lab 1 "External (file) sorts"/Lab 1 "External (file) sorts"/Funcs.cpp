204ca675c30d18fb4001a06cd67af4b82cd7929c
Antony Miroshnichenko <antonymiroshnichenko@MacBook-Pro-Antony.local>
2024-02-20 10:52:32 +0700

    Создана ветка первой лабораторной

---
+++ Lab 1 "External (file) sorts"/Lab 1 "External (file) sorts"/Funcs.cpp
@@ -0,0 +1,8 @@
+//
+//  Funcs.cpp
+//  Lab 1 "External (file) sorts"
+//
+//  Created by Antony Miroshnichenko on 20.02.2024.
+//
+
+#include "Funcs.hpp"
---
+++ Lab 1 "External (file) sorts"/Lab 1 "External (file) sorts"/Funcs.hpp
@@ -0,0 +1,13 @@
+//
+//  Funcs.hpp
+//  Lab 1 "External (file) sorts"
+//
+//  Created by Antony Miroshnichenko on 20.02.2024.
+//
+
+#ifndef Funcs_hpp
+#define Funcs_hpp
+
+#include <stdio.h>
+
+#endif /* Funcs_hpp */
